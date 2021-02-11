0dbb3954a4205be21c1caebad35c901d7477f2fa
Marc BALAGUER <marcbalaguer13@gmail.com>
2021-02-11 22:16:37 +0100

    force

---
+++ frontOffice/addons/navbar.php
@@ -0,0 +1,62 @@
+	<nav class="fh5co-nav" role="navigation">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-3 col-xs-2">
+				</div>
+				<div class="col-md-6 col-xs-6 text-center menu-1">
+					<div id="fh5co-logo"><a href="index.html" style="font-size: 50px;">HIJELIA.</a></div>
+				</div>
+				<div class="col-md-3 col-xs-4 text-right hidden-xs menu-2">
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-3 col-xs-6 text-right hidden-xs menu-2">
+					<ul>
+						<li class="search">
+							<div class="input-group">
+						      <input type="text" placeholder="Recherche ..." >
+						      <span class="input-group-btn">
+						        <button class="btn btn-primary" type="button"><i class="icon-search"></i></button>
+						      </span>
+						    </div>
+						</li>
+					</ul>
+				</div>
+				<div class="col-md-6 col-xs-6 text-center menu-1">
+					<ul>
+						<li class="has-dropdown">
+							<a href="#">Categorie 1</a>
+							<ul class="dropdown">
+								<li><a href="#">SousCat. 1.1</a></li>
+								<li><a href="#">SousCat. 1.2</a></li>
+								<li><a href="#">SousCat. 1.3</a></li>
+							</ul>
+						</li>
+						<li class="has-dropdown">
+							<a href="#">Categorie 2</a>
+							<ul class="dropdown">
+								<li><a href="#">SousCat. 2.1</a></li>
+								<li><a href="#">SousCat. 2.2</a></li>
+								<li><a href="#">SousCat. 2.3</a></li>
+							</ul>
+						</li>
+						<li class="has-dropdown">
+							<a href="#">Categorie 3</a>
+							<ul class="dropdown">
+								<li><a href="#">SousCat. 3.1</a></li>
+								<li><a href="#">SousCat. 3.2</a></li>
+								<li><a href="#">SousCat. 3.3</a></li>
+							</ul>
+						</li>
+						<li><a href="contact.html">Contact</a></li>
+					</ul>
+				</div>
+				<div class="col-md-3 col-xs-4 text-right hidden-xs menu-2">
+					<ul>
+						<li class="shopping-cart"><a href="#" class="cart"><span><small>0</small><i class="icon-shopping-cart"></i></span></a></li>
+					</ul>
+				</div>
+			</div>
+			
+		</div>
+	</nav>
---
+++ frontOffice/index.php
@@ -0,0 +1,236 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>HIJELIA &mdash; Accueil</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="Vetement" />
+	<meta name="author" content="Marc BALAGUER & Lilya RAHBI" />
+
+  	<!-- Facebook and Twitter integration -->
+	<meta property="og:title" content=""/>
+	<meta property="og:image" content=""/>
+	<meta property="og:url" content=""/>
+	<meta property="og:site_name" content=""/>
+	<meta property="og:description" content=""/>
+	<meta name="twitter:title" content="" />
+	<meta name="twitter:image" content="" />
+	<meta name="twitter:url" content="" />
+	<meta name="twitter:card" content="" />
+<?php include 'addons/head.html' ?>
+	</head>
+	<body>
+	<?php include 'addons/navbar.php' ?>
+
+	<div class="fh5co-loader"></div>
+	
+	<div id="page">
+	<aside id="fh5co-hero" class="js-fullheight">
+		<div class="flexslider js-fullheight">
+			<ul class="slides">
+		   	<li style="background-image: url(images/img_bg_1.jpg);">
+		   		
+		   	</li>
+		  	</ul>
+	  	</div>
+	</aside>
+
+	<div id="fh5co-services" class="fh5co-bg-section">
+		<div class="container">
+			<div class="row animate-box">
+				<div class="col-md-8 col-md-offset-2 text-center fh5co-heading">
+					<span>Our selection</span>
+					<h2>Notre selection.</h2>
+					<p>Voici la selection des produits pouvant vous intéresser.</p>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-4 text-center animate-box">
+					<div class="product">
+						<div class="product-grid" style="background-image:url(images/product-1.jpg);">
+							<div class="inner">
+								<p>
+									<a href="single.html" class="icon"><i class="icon-shopping-cart"></i></a>
+									<a href="single.html" class="icon"><i class="icon-eye"></i></a>
+								</p>
+							</div>
+						</div>
+						<div class="desc">
+							<h3><a href="single.html">Produit 1</a></h3>
+							<span class="price">100 €</span>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-4 text-center animate-box">
+					<div class="product">
+						<div class="product-grid" style="background-image:url(images/product-2.jpg);">
+							<span class="sale">Nouveauté</span>
+							<div class="inner">
+								<p>
+									<a href="single.html" class="icon"><i class="icon-shopping-cart"></i></a>
+									<a href="single.html" class="icon"><i class="icon-eye"></i></a>
+								</p>
+							</div>
+						</div>
+						<div class="desc">
+							<h3><a href="single.html">Produit 2</a></h3>
+							<span class="price">50 €</span>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-4 text-center animate-box">
+					<div class="product">
+						<div class="product-grid" style="background-image:url(images/product-3.jpg);">
+							<div class="inner">
+								<p>
+									<a href="single.html" class="icon"><i class="icon-shopping-cart"></i></a>
+									<a href="single.html" class="icon"><i class="icon-eye"></i></a>
+								</p>
+							</div>
+						</div>
+						<div class="desc">
+							<h3><a href="single.html">Produit 3</a></h3>
+							<span class="price">75 €</span>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-4 text-center animate-box">
+					<div class="product">
+						<div class="product-grid" style="background-image:url(images/product-4.jpg);">
+							<span class="sale">Nouveauté</span>
+							<div class="inner">
+								<p>
+									<a href="single.html" class="icon"><i class="icon-shopping-cart"></i></a>
+									<a href="single.html" class="icon"><i class="icon-eye"></i></a>
+								</p>
+							</div>
+						</div>
+						<div class="desc">
+							<h3><a href="single.html">Produit 4</a></h3>
+							<span class="price">15 €</span>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-4 text-center animate-box">
+					<div class="product">
+						<div class="product-grid" style="background-image:url(images/product-5.jpg);">
+							<div class="inner">
+								<p>
+									<a href="single.html" class="icon"><i class="icon-shopping-cart"></i></a>
+									<a href="single.html" class="icon"><i class="icon-eye"></i></a>
+								</p>
+							</div>
+						</div>
+						<div class="desc">
+							<h3><a href="single.html">Produit 5</a></h3>
+							<span class="price">100 €</span>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-4 text-center animate-box">
+					<div class="product">
+						<div class="product-grid" style="background-image:url(images/product-6.jpg);">
+							<span class="sale">Nouveauté</span>
+							<div class="inner">
+								<p>
+									<a href="single.html" class="icon"><i class="icon-shopping-cart"></i></a>
+									<a href="single.html" class="icon"><i class="icon-eye"></i></a>
+								</p>
+							</div>
+						</div>
+						<div class="desc">
+							<h3><a href="single.html">Produit 6</a></h3>
+							<span class="price">200 €</span>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+		<div id="fh5co-counter" class="fh5co-bg fh5co-counter" style="background-image:url(images/img_bg_5.jpg);">
+		<div class="container">
+			<div class="row">
+				<div class="display-t">
+					<div class="display-tc">
+						<div class="col-md-3 col-sm-6 animate-box">
+							<div class="feature-center">
+								<span class="icon">
+									<i class="icon-eye"></i>
+								</span>
+
+								<span class="counter js-counter" data-from="0" data-to="30" data-speed="5000" data-refresh-interval="50">1</span>
+								<span class="counter-label">Pays</span>
+
+							</div>
+						</div>
+						<div class="col-md-3 col-sm-6 animate-box">
+							<div class="feature-center">
+								<span class="icon">
+									<i class="icon-shopping-cart"></i>
+								</span>
+
+								<span class="counter js-counter" data-from="0" data-to="200" data-speed="5000" data-refresh-interval="50">1</span>
+								<span class="counter-label">Clients sastifaits</span>
+							</div>
+						</div>
+						<div class="col-md-3 col-sm-6 animate-box">
+							<div class="feature-center">
+								<span class="icon">
+									<i class="icon-shop"></i>
+								</span>
+								<span class="counter js-counter" data-from="0" data-to="250" data-speed="5000" data-refresh-interval="50">1</span>
+								<span class="counter-label">Produits</span>
+							</div>
+						</div>
+						<div class="col-md-3 col-sm-6 animate-box">
+							<div class="feature-center">
+								<span class="icon">
+									<i class="icon-clock"></i>
+								</span>
+
+								<span class="counter js-counter" data-from="0" data-to="24" data-speed="5000" data-refresh-interval="200">1</span>
+								<span class="counter-label">h / 24 h</span>
+								
+
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div id="fh5co-started">
+		<div class="container">
+			<div class="row animate-box">
+				<div class="col-md-8 col-md-offset-2 text-center fh5co-heading">
+					<h2>Newsletter</h2>
+					<p>Inscrivez-vous pour recevoir les dernières nouvelles et mises à jour directement dans votre boîte de réception</p>
+				</div>
+			</div>
+			<div class="row animate-box">
+				<div class="col-md-8 col-md-offset-2">
+					<form class="form-inline">
+						<div class="col-md-6 col-sm-6">
+							<div class="form-group">
+								<label for="email" class="sr-only">Email</label>
+								<input type="email" class="form-control" id="email" placeholder="Email">
+							</div>
+						</div>
+						<div class="col-md-6 col-sm-6">
+							<button type="submit" class="btn btn-default btn-block">Inscription</button>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+	</div>
+	<?php include 'addons/footer.html' ?>
+
+</body>
+</html>
+
